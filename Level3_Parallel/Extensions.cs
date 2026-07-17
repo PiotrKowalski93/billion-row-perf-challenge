2befdb08c93734b03e170c0e13411456ecde3e08
Piotr Kowalski <piotrkow93@gmail.com>
2026-07-17 18:40:40 +0200

    Extension method for merging stats, final multithread

---
+++ Level3_Parallel/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Shared;
+
+namespace Level3_Parallel
+{
+    public static class Extensions
+    {
+        public static void Merge(this StationStats a, StationStats b)
+        {
+            a.Min = Math.Min(a.Min, b.Min);
+            a.Max = Math.Max(a.Max, b.Max);
+            a.Sum += b.Sum;
+            a.Count += b.Count;
+        }
+    }
+}
--- Level3_Parallel/Program.cs
+++ Level3_Parallel/Program.cs
@@ -1,14 +1,18 @@
-﻿using Shared;
+﻿using Level3_Parallel;
+using Shared;
 using System.Diagnostics;
+using System.Text;
+
+var FilePath = GlobalConstants.FilePath_10M;
 
 Console.WriteLine("====== Level 3: Parallel Implementation ======");
-Console.WriteLine($"File: {GlobalConstants.FilePath_10M}");
+Console.WriteLine($"File: {FilePath}");
 Console.WriteLine();
 
 // Verify if the file exists before attempting to read it
-if (!File.Exists(GlobalConstants.FilePath_10M))
+if (!File.Exists(FilePath))
 {
-    Console.WriteLine($"File not found: {GlobalConstants.FilePath_10M}");
+    Console.WriteLine($"File not found: {FilePath}");
 }
 
 // Force GC before measurement
@@ -18,6 +22,86 @@ GC.Collect();
 
 var stopwatch = Stopwatch.StartNew();
 
+var threadCount = Environment.ProcessorCount;
+var chunks = ComputeChunks(FilePath, threadCount);
+
+Console.WriteLine($"Thread Count: {threadCount}");
+Console.WriteLine();
+
+var threadLocalDics = new Dictionary<string, StationStats>[threadCount];
+var lineCounters = new int[threadCount];
+
+Parallel.For(0, threadCount, threadIndex =>
+{
+    var localDic = new Dictionary<string, StationStats>(capacity: GlobalConstants.ExpectedStationCount / threadCount);
+    var localLineCounter = 0;
+
+    var startByte = chunks[threadIndex];
+    var endByte = chunks[threadIndex + 1];
+    var chunkSize = (int)(endByte - startByte);
+
+    var buffer = new byte[chunkSize];
+    using (var stream = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+    {
+        stream.Seek(startByte, SeekOrigin.Begin);
+        stream.ReadExactly(buffer, 0, chunkSize);
+    }
+
+    var text = Encoding.UTF8.GetString(buffer);
+    var lineStart = 0;
+
+    for(int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == '\n')
+        {
+            var lineEnd = i;
+
+            ProcessLine(text.AsSpan(lineStart, lineEnd - lineStart), localDic);
+            localLineCounter++;
+            
+            lineStart = i + 1;
+        }
+    }
+
+    threadLocalDics[threadIndex] = localDic;
+    lineCounters[threadIndex] = localLineCounter;
+});
+
+var finalDict = new Dictionary<string, StationStats>(capacity: 413);
+foreach (var localDict in threadLocalDics)
+{
+    foreach (var kvp in localDict)
+    {
+        if (!finalDict.TryGetValue(kvp.Key, out var stats))
+        {
+            stats = new StationStats();
+            finalDict[kvp.Key] = stats;
+        }
+
+        stats.Merge(kvp.Value);
+    }
+}
+
+var totalLines = lineCounters.Sum();
+
+stopwatch.Stop();
+
+static void ProcessLine(ReadOnlySpan<char> line, Dictionary<string, StationStats> localDic)
+{
+    var separationIndex = line.IndexOf(";");
+    if(separationIndex < 0) return;
+
+    var stationName = line[..separationIndex].ToString();
+    var temperature = double.Parse(line[(separationIndex + 1)..]);
+
+    if (!localDic.TryGetValue(stationName, out var stats))
+    {
+        stats = new StationStats();
+        localDic[stationName] = stats;
+    }
+
+    stats.Update(temperature);
+}
 
 static long[] ComputeChunks(string filePath, int threadCount)
 {
