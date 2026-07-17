1e81709ee5898d0580187211106ec4f4e28bb078
Piotr Kowalski <piotrkow93@gmail.com>
2026-07-17 08:23:47 +0200

    Benchmarks init

---
+++ Benchmarks/Parallel_Dict_Benchmarks.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    [BenchmarkCategory("Concurrency")]
+    public class Parallel_Dict_Benchmarks
+    {
+        [Params(10_000, 100_000, 1_000_000, 10_000_000)]
+        public int MeasurementCount { get; set; }
+
+        private List<(string StationName, double Temperature)> measurements = [];
+        private readonly object _lockObj = new();
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var random = new Random(0197);
+
+            var stations = new string[413];
+            for (int i = 0; i < stations.Length; i++)
+            {
+                stations[i] = $"Station_{i:D3}";
+            }
+
+            for (int i = 0; i < MeasurementCount; i++)
+            {
+                var station = stations[random.Next(stations.Length)];
+                var temperature = random.NextDouble() * 40 - 10;
+                measurements.Add((station, temperature));
+            }
+        }
+
+    }
+}
--- Level3_Parallel/Program.cs
+++ Level3_Parallel/Program.cs
@@ -21,5 +21,42 @@ var stopwatch = Stopwatch.StartNew();
 
 static long[] ComputeChunks(string filePath, int threadCount)
 {
+    var fileInfo = new FileInfo(filePath);
+    var fileSize = fileInfo.Length;
 
+    var chunks = new long[threadCount + 1];
+
+    // We do not have BOM in this file, so we can safely divide the file into equal parts
+
+    chunks[0] = 0;
+    chunks[threadCount] = fileSize;
+    var chunkSize = fileSize / threadCount;
+
+    using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+    for(int i = 1; i < threadCount; i++)
+    {
+        // End of boundary for each thread
+        var targetPosition = i * chunkSize;
+
+        fileStream.Seek(targetPosition, SeekOrigin.Begin);
+
+        int b;
+        while((b = fileStream.ReadByte()) != -1)
+        {
+            // New line character
+            if (b == '\n') 
+            {
+                chunks[i] = fileStream.Position;
+                break;
+            }
+        }
+
+        if(b == -1)
+        {
+            chunks[i] = fileSize;
+        }
+    }
+
+    return chunks;
 }
